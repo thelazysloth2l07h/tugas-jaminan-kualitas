5dbeba0fddc3dbf2cc197f91a745d049925d3561
Hako Yamasaki <thelazysloth2l07h@gmail.com>
2026-06-17 22:22:28 +0700

    test: sengaja menggagalkan uji untuk validasi sistem ci

--- math.test.js
+++ math.test.js
@@ -1,5 +1,6 @@
 const { tambah } = require('./math.js');
 
 test('Menguji fungsi tambah: 2 + 3 harus sama dengan 5', () => {
-    expect(tambah(2, 3)).toBe(5);
+    // Sengaja digagalkan: 2 + 3 diekspektasikan menjadi 99 (pasti salah)
+    expect(tambah(2, 3)).toBe(99); 
 });
